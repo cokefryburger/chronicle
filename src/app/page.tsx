e94ab652c87fafebcd23f8ae04d0058a9fe440b5
cokefryburger <122137054+cokefryburger@users.noreply.github.com>
2024-07-06 19:10:47 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black text-white">
+     <div className="flex w-full space-x-5">
+      <h1 className="text-5xl font-gothic">Chronicle</h1>
+     </div>
+    </main>
+  );
+}
