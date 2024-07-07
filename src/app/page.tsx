15a1165532a39d0e7a64f0466922cf9aa58588de
cokefryburger <122137054+cokefryburger@users.noreply.github.com>
2024-07-07 11:59:59 -0400

    add char title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import * as React from "react";
 
-interface character {
+class Character {
   name:      string;
-  title:     rank;
+  rank:      rank;
   stats:     stat[];
-  journeyed: Date;
+  journeyed: number;
+
+  constructor(name: string, rank: rank, stats: stat[] = [], journeyed: number = Date.now()) {
+    this.name      = name;
+    this.rank      = rank;
+    this.stats     = stats;
+    this.journeyed = journeyed;
+  }
+
+  get title(): string {
+    return this.rank.suffix ? `${this.name} ${this.rank.title}` : `${this.rank.title} ${this.name}`;
+  }
 }
 
 interface stat {
@@ -16,18 +27,26 @@ interface stat {
 interface talisman {
   level:    number;
   rarity:   string;
-  consumed: Date;
+  consumed: number;
 }
 
-interface rank {
-  name:    string;
-  color:   string;
-  title?:  string;
-  xp:      number;
-  suffix?: boolean
+class rank {
+  name:   string;
+  rarity: rarity;
+  title:  string;
+  suffix: boolean = false;
+  xp:     number = 0;
+
+  constructor(name: string, title: string, rarity: rarity, opts: { xp: number, suffix?: boolean }) {
+    this.name   = name;
+    this.title  = title;
+    this.rarity = rarity;
+    this.xp     = opts.xp;
+    this.suffix = opts.suffix ?? false;
+  }
 }
 
-enum color {
+enum rarity {
   junk      = "#323232",
   poor      = "#646464",
   normal    = "#989084",
@@ -41,64 +60,36 @@ enum color {
   hightower = "#1b4a0f",
 }
 
-const ranks: rank[] = [
-  {
-    name: "Peasant",
-    title: "Peasant",
-    color: color.poor,
-    xp: 0,
-  },
-  {
-    name: "Squire",
-    title: "Squire",
-    color: color.uncommon,
-    xp: 100,
-  },
-  {
-    name: "Knight",
-    title: "Ser",
-    color: color.rare,
-    xp: 5000,
-  },
-  {
-    name: "Lord",
-    title: "Lord",
-    color: color.epic,
-    xp: 100000,
-  },
-  {
-    name: "Royal",
-    title: "Regent",
-    color: color.legendary,
-    xp: 1000000,
-  },
-  {
-    name: "Mythical",
-    title: "Mythic",
-    color: color.unique,
-    xp: 5000000
-  },
-  {
-    name: "Conqueror",
-    title: "the Conqueror",
-    color: color.mythic,
-    xp: 10000000,
-    suffix: true
-  }
-];
+const ranks = {
+  peasant:   new rank("Peasant", "Peasant", rarity.poor, { xp: 0 }),
+  squire:    new rank("Squire", "Squire", rarity.uncommon, { xp: 100 }),
+  knight:    new rank("Knight", "Ser", rarity.rare, { xp: 5000 }),
+  lord:      new rank("Lord", "Lord", rarity.epic, { xp: 100000 }),
+  royal:     new rank("Royal", "Regent", rarity.legendary, { xp: 1000000 }),
+  mythical:  new rank("Mythical", "Mythic", rarity.unique, { xp: 5000000 }),
+  conqueror: new rank("Conqueror", "the Conqueror", rarity.mythic, { xp: 10000000, suffix: true }),
+};
 
 export default function Home() {
+  const character = new Character("Napoleon", ranks.lord, []);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-12 p-24 bg-black text-white">
-      <div className="flex w-full space-x-5">
+    <main
+      className={`
+        flex min-h-screen flex-col space-y-12 p-24
+        bg-black text-white selection:text-red-800
+      `}
+    >
+      <div className="w-fit">
         <h1 className="text-6xl font-gothic">Chronicle</h1>
+        <h2 className="text-1xl font-gaelic pl-3">of {character.title}</h2>
       </div>
       <div className="flex flex-row space-x-4 w-full justify-center">
-        {ranks.map((rank, id) => (
+        {Object.values(ranks).map((rank, id) => (
           <p
             key={id}
             className="text-3xl font-gaelic"
-            style={{ color: rank.color }}
+            style={{ color: rank.rarity }}
           >
             {rank.name}
           </p>
